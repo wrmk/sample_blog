5e14d5eb4a41310a7deb5ff08821513060348ab4
wrmk <elmakan@gmail.com>
2020-07-12 19:38:33 +0300

    save contact to db

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,7 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  #render - функция plain: params[:article].inspect - параметр функции plain: - ключ хеша params[:article].inspect - значение хеша
   def create
     render plain: params[:article].inspect
   end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,6 +4,17 @@ class ContactsController < ApplicationController
   end
 
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
   end
+
+  # метод в котором указываем какие параметры со страницы можно взять
+  # в private чтобы к нему нельзя было обратиться извне
+  private
+  
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 
 end
