8006d407f4b0ef512a3726141ae5c7135fa0d790
wrmk <elmakan@gmail.com>
2020-07-17 20:42:13 +0300

    require gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
   
+  devise_for :users
+  root to: "home#index"
 
+  get 'home/index'
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
