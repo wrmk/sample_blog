550004ebded441522c752e3bf72d7d424948c0cd
wrmk <elmakan@gmail.com>
2020-07-18 19:37:37 +0300

    create username for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if :devise_controller? # выполнить функцию такую то ДО выполнения контроллера, если функция вызывается для devise
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username #добавили запрос на указание имя пользователя при логине
+  end
 end
